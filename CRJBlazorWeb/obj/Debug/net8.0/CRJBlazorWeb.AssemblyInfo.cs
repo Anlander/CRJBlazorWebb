31dafe0186d4b030293a00aa7c0b4f701c304af8
Razvan-Petru Nechifor <144475822+nrazv@users.noreply.github.com>
2025-03-15 11:05:21 +0100

    Update

--- CRJBlazorWeb/obj/Debug/net8.0/CRJBlazorWeb.AssemblyInfo.cs
+++ CRJBlazorWeb/obj/Debug/net8.0/CRJBlazorWeb.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRJBlazorWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ddb91a09bd6957cbe737ed1d7edeb1754a3755a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0baff140c9b65cc00203776d1d240f3ea57b5cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRJBlazorWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRJBlazorWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
